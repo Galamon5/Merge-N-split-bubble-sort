209c36e4992116c43f04b1699c81dab66d9a717e
Juan Carlos Galaviz Morua <galamon5@hotmail.com>
2017-10-08 20:28:18 -0500

    Programa terminado, se agrego el bubble sort y se implemento la division del arreglo

--- ordenamiento_divyvence.py
+++ ordenamiento_divyvence.py
@@ -5,8 +5,9 @@ def main():
     size = int(input("Introduce el tamaño del arreglo\n"))
     div = int(input("Introduce el numero de divisiones\n"))
     arrayNum = createArray(size)
-    print("El arreglo original es",arrayNum)
-    arraySort(arrayNum,div)
+    print("El arreglo original es: ",arrayNum)
+    finalSort = arraySort(arrayNum,div)
+    print("El arreglo ordenado es: ",finalSort)
 
 def createArray(size):
     arrayAux = []
@@ -16,25 +17,58 @@ def createArray(size):
     return arrayAux
 
 def arraySort(arrayNum,div):
-    size = len(arrayNum)
-    numDiv = int(size/div)
-    if(size<=1):
-        print("Ordenado! \n",arrayNum)
-    else:
-        if(size<=div):
-            print("El numero de divisiones es mayor o igual que el tamaño del arreglo\n")
-            arraySort(arrayNum,div-1)
+    z = 0
+    sortArray = []
+    helpArray = []
+    sizeArray = len(arrayNum)
+    numDiv = int(sizeArray/div)
+    #print(numDiv)
+    if(not(sizeArray<=div)):
+        #print("Se puede dividir!")
+        if(sizeArray%div == 0):
+            for k in range(numDiv):
+                helpArray.append(k)
+            for i in range(div):
+                for j in range(numDiv):
+                    helpArray[j] = arrayNum[z]
+                    z = z+1
+                #print("rama: ", helpArray)
+                arraySort(helpArray,div)
+                bubbleSort(helpArray)
+                for l in range(len(helpArray)):
+                    sortArray.append(helpArray[l])
+            bubbleSort(sortArray)
         else:
-            print("Se puede dividir!\n")
-            for i in range(div-1):
-                if()
+            numDiv = numDiv+1
+            for k in range(numDiv):
+                helpArray.append(k)
+            for i in range(div):
+                for j in range(numDiv):
+                    if(z<sizeArray):
+                        helpArray[j] = arrayNum[z]
+                        z = z+1
+                    else:
+                        helpArray.pop()
+                #print("rama: ", helpArray)
+                arraySort(helpArray,div)
+                bubbleSort(helpArray)
+                for l in range(len(helpArray)):
+                    sortArray.append(helpArray[l])
+            bubbleSort(sortArray)
+        finalSort = sortArray
+        return finalSort
+        #print("rama ordenada: ",finalSort)
 
+def bubbleSort( A ):
+    for i in range( len( A ) ):
+        for k in range( len( A ) - 1, i, -1 ):
+            if ( A[k] < A[k - 1] ):
+                swap( A, k, k - 1 )
 
-def merge(arrayNum,):
-    print("Ordenando")
-
-def bubbleSort():
-    print("Ordenando")
+def swap( A, x, y ):
+    tmp = A[x]
+    A[x] = A[y]
+    A[y] = tmp
 
 main()
 input("Pulsa una tecla para continuar...")
